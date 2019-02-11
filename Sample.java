9a7696b01f39dcf8fc8212d4c9c7122183070887
Naveen <adith.naveen@gmail.com>
2019-02-12 03:51:47 +0530

    added conditional check

--- Sample.java
+++ Sample.java
@@ -2,6 +2,19 @@ class Sample{
 	public static void main(String args[]){
 		System.out.println("Hello from Sublime");
 		System.out.pritln("Added new line for second time"); 
+
+
+
+		///// this code is in new branch 
+		int a = 10; 
+		int b = 20; 
+
+		if(a> b){
+			System.out.println("Hi");
+		}else{
+			System.out.println("Bye"); 
+		}
+
 	}
 
 }
